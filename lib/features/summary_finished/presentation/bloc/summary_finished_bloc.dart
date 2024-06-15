feaa5fb1460f6c342fbd5badcaa6e02e25b1db26
Juan Esteban G <juanesteban8956@gmail.com>
2024-06-15 10:18:16 -0500

    structuring of folder Summary Finished

---
+++ lib/features/summary_finished/presentation/bloc/summary_finished_bloc.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part 'summary_finished_event.dart';
+part 'summary_finished_state.dart';
+
+class SummaryFinishedBloc extends Bloc<SummaryFinishedEvent, SummaryFinishedState> {
+  //define controllers login
+
+  //get data
+
+  SummaryFinishedBloc() : super(const SummaryFinishedState());
+}
---
+++ lib/features/summary_finished/presentation/bloc/summary_finished_state.dart
@@ -0,0 +1,10 @@
+part of 'summary_finished_bloc.dart';
+
+class SummaryFinishedState extends Equatable {
+  const SummaryFinishedState();
+
+  SummaryFinishedState copyWith() => const SummaryFinishedState();
+
+  @override
+  List<Object?> get props => [];
+}
