087700b8ba8f14e58f17c9162c7974980217afba
Juan Esteban G <juanesteban8956@gmail.com>
2024-06-15 09:58:24 -0500

    structuring of folder Configuration

---
+++ lib/features/configuration/presentation/bloc/configuration_bloc.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part 'configuration_event.dart';
+part 'configuration_state.dart';
+
+class ConfigurationBloc extends Bloc<ConfigurationEvent, ConfigurationState> {
+  //define controllers login
+
+  //get data
+
+  ConfigurationBloc() : super(const ConfigurationState());
+}
